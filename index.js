f5ed75799d2feea55ce07138a7bc952a582514c7
Maksym Dombrov <maksymdombrov@gmail.com>
2024-07-25 07:22:36 +0300

    add draw shape functionality + update project setup

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+
+function drawShape(radius, inset, n) {
+  ctx.beginPath();
+  ctx.save();
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.moveTo(0, 0 - radius);
+
+  for (let i = 0; i < n; i++) {
+    ctx.rotate(Math.PI / n);
+    ctx.lineTo(0, 0 - radius * inset);
+    ctx.rotate(Math.PI / n);
+    ctx.lineTo(0, 0 - radius);
+  }
+
+  ctx.restore();
+  ctx.closePath();
+  ctx.stroke();
+}
+drawShape(100, 0.5, 6);
