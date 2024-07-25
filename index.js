356907798975d6795b24fd54e9b993355d5aa502
Maksym Dombrov <maksymdombrov@gmail.com>
2024-07-25 07:46:51 +0300

    add painting on mouse move

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-ctx.fillStyle = 'blue';
-ctx.lineWidth = 5;
+ctx.fillStyle = 'grey';
+ctx.strokeStyle = 'purple';
+ctx.lineWidth = 2;
+ctx.shadowOffsetX = 10;
+ctx.shadowOffsetY = 10;
+ctx.shadowBlur = 10;
+ctx.shadowColor = 'black';
 
-function drawShape(radius, inset, n) {
+function drawShape(x, y, radius, inset, n) {
   ctx.beginPath();
   ctx.save();
-  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.translate(x, y);
   ctx.moveTo(0, 0 - radius);
 
   for (let i = 0; i < n; i++) {
@@ -23,4 +28,8 @@ function drawShape(radius, inset, n) {
   ctx.stroke();
   ctx.fill();
 }
-drawShape(100, 0.5, 6);
+drawShape(100, 100, 100, 0.3, 7);
+
+window.addEventListener('mousemove', e => {
+  drawShape(e.x, e.y, 150, 0.3, 7);
+});
