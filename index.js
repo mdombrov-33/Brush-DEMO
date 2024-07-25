aae283c55cefdec09fd5be50e2d07688609e1bbf
Maksym Dombrov <maksymdombrov@gmail.com>
2024-07-25 11:08:46 +0300

    add shape randomization functionality

--- index.js
+++ index.js
@@ -35,10 +35,23 @@ const radius = 50;
 const inset = 0.5;
 const n = 10;
 
+// Generate random values for shape parameters
+const randomRadius1 = Math.random() * 100;
+const randomInset1 = Math.random();
+const randomSides1 = Math.floor(Math.random() * 10) + 3;
+
+const randomRadius2 = Math.random() * 100;
+const randomInset2 = Math.random();
+const randomSides2 = Math.floor(Math.random() * 10) + 3;
+
+const randomRadius3 = Math.random() * 100;
+const randomInset3 = Math.random();
+const randomSides3 = Math.floor(Math.random() * 10) + 3;
+
 //Shapes preview
-drawShape(70, 70, radius * 0.95, 0.2, 7);
-drawShape(120, 120, radius * 0.8, inset, 4);
-drawShape(180, 180, radius + 0.5, inset, 5);
+drawShape(60, 70, randomRadius1, randomInset1, randomSides1);
+drawShape(150, 160, randomRadius2, randomInset2, randomSides2);
+drawShape(240, 250, randomRadius3, randomInset3, randomSides3);
 
 let angle = 0;
 
@@ -52,19 +65,37 @@ window.addEventListener('mousemove', e => {
     ctx.rotate(angle);
     ctx.fillStyle = '#683bd9';
     ctx.strokeStyle = 'black';
-    drawShape(radius / 0.5, radius / 5, radius * 0.95, 0.2, 7);
+    drawShape(
+      radius / 0.5,
+      radius / 5,
+      randomRadius1,
+      randomInset1,
+      randomSides1
+    );
 
     //Second shape
     ctx.rotate(-angle * 3);
     ctx.fillStyle = '#b2a6ce';
     ctx.strokeStyle = 'purple';
-    drawShape(radius / 1.25, radius / 5, radius, inset, 4);
+    drawShape(
+      radius / 1.25,
+      radius / 5,
+      randomRadius2,
+      randomInset2,
+      randomSides2
+    );
 
     //Third shape
     ctx.rotate(-angle * 3);
     ctx.fillStyle = 'purple';
     ctx.strokeStyle = 'black';
-    drawShape(radius / 1.5, radius / 5, radius + 0.5, inset, 5);
+    drawShape(
+      radius / 1.5,
+      radius / 5,
+      randomRadius3,
+      randomInset3,
+      randomSides3
+    );
 
     angle += 0.1;
     ctx.restore();
